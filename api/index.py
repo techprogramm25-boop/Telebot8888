2745c1eb10a03f7e2d137d8e94b098d9a1e3364c
Yusufxonpro <techprogramm25@gmail.com>
2026-09-22 12:31:37 +0500

    Update index.py

--- api/index.py
+++ api/index.py
@@ -1,10 +1,8 @@
 import sys
 import os
 
-# Asosiy papkani yo'lga qo'shish
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 from main import app
 
-# Vercel serverless funksiyasi uchun handler
 handler = app
